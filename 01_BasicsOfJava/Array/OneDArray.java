62a4a01e5f7e81a4711e65f0e1f9ed06670b78cd
Sachin Shetkar <ssshetkar2001@gmail.com>
2026-04-16 23:59:58 +0530

    Arrays

---
+++ 01_BasicsOfJava/Array/OneDArray.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class OneDArray {
+    public static void main(String[] args) {
+
+        //Declaration of array
+        int[] rollNums = new int[3];
+        // 101,102,103
+        rollNums[0] = 101;
+        rollNums[1] = 102;
+        rollNums[2] = 103;
+
+        //print using loop
+
+        for(int i=0;i<rollNums.length;i++)
+            System.out.print(rollNums[i] + " ");
+
+
+        
+    }
+}
