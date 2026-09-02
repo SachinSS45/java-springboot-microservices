afaf08d2ccc931060e4aae1bff9fa99f469170d4
Subramanyam9028 <sbjadhav9028@gmail.com>
2026-09-02 17:28:04 +0530

    first-api

---
+++ SpringBoot/Revision/REST_API/first-rest-api/src/main/java/com/sachin/first_rest_api/User.java
@@ -0,0 +1,16 @@
+package com.sachin.first_rest_api;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    
+
+}
