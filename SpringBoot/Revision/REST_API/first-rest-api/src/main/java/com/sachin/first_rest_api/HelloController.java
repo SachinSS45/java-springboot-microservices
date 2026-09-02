64f76e6224c4740fc4e4042ae7aae38e0a3cb139
SachinSS45 <ssshetkar2001@gmail.com>
2026-09-02 18:00:38 +0530

    Java to JSON internally Jackson

--- SpringBoot/Revision/REST_API/first-rest-api/src/main/java/com/sachin/first_rest_api/HelloController.java
+++ SpringBoot/Revision/REST_API/first-rest-api/src/main/java/com/sachin/first_rest_api/HelloController.java
@@ -1,13 +1,23 @@
 package com.sachin.first_rest_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/app")
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello : Your First API";
+    }
+
+    //@GetMapping("/user")
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User getUser(){
+        User user = new User(1,"Sachin","Shetkar");
+        return user;
     }
 }
--- SpringBoot/Revision/REST_API/first-rest-api/src/main/java/com/sachin/first_rest_api/User.java
+++ SpringBoot/Revision/REST_API/first-rest-api/src/main/java/com/sachin/first_rest_api/User.java
@@ -11,6 +11,28 @@ public class User {
         this.name = name;
         this.email = email;
     }
-    
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
