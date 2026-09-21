63e38870c72e24e77850592a576bacf40977c555
Sachin Shetkar <ssshetkar2001@gmail.com>
2026-09-21 16:47:38 +0300

    3.6 : Entity lifecycle,Entity Manager and Persistence Context

--- CodingShuttle/03_SpringDataJPA/HospitalManagmentSystem/src/main/java/com/sachin/hospitalmanagmentsystem/services/PatientService.java
+++ CodingShuttle/03_SpringDataJPA/HospitalManagmentSystem/src/main/java/com/sachin/hospitalmanagmentsystem/services/PatientService.java
@@ -3,6 +3,7 @@ package com.sachin.hospitalmanagmentsystem.services;
 import com.sachin.hospitalmanagmentsystem.dto.IPatientInfo;
 import com.sachin.hospitalmanagmentsystem.entity.Patient;
 import com.sachin.hospitalmanagmentsystem.repository.PatientRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,28 @@ public class PatientService implements IPatientService{
     @Override
     public List<IPatientInfo> getAllPatientsInfo() {
         return patientRepository.getAllPatientsInfo();
+    }
+
+    public void testPatientTransaction(){
+        Patient p1 = patientRepository.findById(1L).orElseThrow();
+        Patient p2 = patientRepository.findById(1L).orElseThrow();
+        //there will two calls to db for same id
+        System.out.println(p1 + " " + p2);
+        System.out.println(p1==p2);//false
+    }
+
+    //now if I use transactional context see : all the operations will happen in same persistence context
+    @Transactional
+    public void testPatientTransaction2(){
+        Patient p1 = patientRepository.findById(1L).orElseThrow();//it is not present in persisitence context so it will add that in
+        //in persistence context
+
+        //now it will check in persistence context is it already present yes then it will use that only it will not call again to db
+        Patient p2 = patientRepository.findById(1L).orElseThrow();
+
+        //there will two calls to db for same id
+        System.out.println(p1 + " " + p2);
+        System.out.println(p1==p2);//true
     }
 
 }
--- CodingShuttle/03_SpringDataJPA/HospitalManagmentSystem/src/test/java/com/sachin/hospitalmanagmentsystem/PatientServiceTest.java
+++ CodingShuttle/03_SpringDataJPA/HospitalManagmentSystem/src/test/java/com/sachin/hospitalmanagmentsystem/PatientServiceTest.java
@@ -2,7 +2,9 @@ package com.sachin.hospitalmanagmentsystem;
 
 import com.sachin.hospitalmanagmentsystem.dto.BloodGroupStats;
 import com.sachin.hospitalmanagmentsystem.dto.CPatientInfo;
+import com.sachin.hospitalmanagmentsystem.entity.Patient;
 import com.sachin.hospitalmanagmentsystem.repository.PatientRepository;
+import com.sachin.hospitalmanagmentsystem.services.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,8 +15,8 @@ import java.util.List;
 public class PatientServiceTest {
     @Autowired
     private PatientRepository patientRepository;
-
-
+    @Autowired
+    private PatientService patientService;
     @Test
     public void testPatient(){
        // List<CPatientInfo> patientList = patientRepository.getAllPatientsInfoConcrete();
@@ -22,7 +24,9 @@ public class PatientServiceTest {
 //        for(BloodGroupStats c : patientList){
 //            System.out.println(c);
 //        }
-        int recordsUpdated = patientRepository.updatePatientNameWithId("Sachin Shetkar", 1L);
-        System.out.println(recordsUpdated);
+//        int recordsUpdated = patientRepository.updatePatientNameWithId("Sachin Shetkar", 1L);
+//        System.out.println(recordsUpdated);
+
+        patientService.testPatientTransaction();
     }
 }
